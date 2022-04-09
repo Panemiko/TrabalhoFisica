71e1aeaee6f321a573f2f56a93f4da2d28e7b7cc
Panem <titoluccao@gmail.com>
2022-04-09 20:10:25 -0300

    feat: add Viewer and App support for room naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import './App.css'
 import React from 'react'
+import Viewer from './components/Viewer'
 
 function App() {
-    return <div></div>
+    return (
+        <div>
+            <Viewer room={'BALCONY'} />
+        </div>
+    )
 }
 
 export default App
--- src/components/Viewer.tsx
+++ src/components/Viewer.tsx
@@ -1,11 +1,13 @@
 import { PROJECTION_TYPE, PanoViewer } from '@egjs/react-view360'
 import React from 'react'
+import type { RoomsName } from '../hooks/useRooms'
+import useRooms from '../hooks/useRooms'
 
-const Viewer: React.FC<ViewerProps> = ({ image }) => {
+const Viewer: React.FC<ViewerProps> = ({ room }) => {
     return (
         <PanoViewer
             tag='div'
-            image={image}
+            image={useRooms(room).image}
             useZoom={false}
             projectionType={PROJECTION_TYPE.CUBEMAP}
             cubemapConfig={{
@@ -16,7 +18,7 @@ const Viewer: React.FC<ViewerProps> = ({ image }) => {
 }
 
 export type ViewerProps = {
-    image: string
+    room: RoomsName
 }
 
 export default Viewer
