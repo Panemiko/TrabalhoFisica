82901f37ee2c4a196d9c29a7caab1b3c58167482
Panem <titoluccao@gmail.com>
2022-04-10 01:44:17 -0300

    feat: add Room file list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css'
 import React from 'react'
+import RoomButton from './components/RoomButton'
 import Viewer from './components/Viewer'
+import rooms from '../ROOMS.json'
 
 function App() {
     return (
-        <div>
-            <Viewer room={'BALCONY'} />
-        </div>
+        <React.Fragment>
+            <div className='.menu'>
+                {rooms.map((room) => {
+                    return (
+                        <RoomButton
+                            key={room.id}
+                            room={room}
+                            onClick={(room) => console.log(room.name)}
+                        />
+                    )
+                })}
+            </div>
+            <div>{/* <Viewer room={'BALCONY'} /> */}</div>
+        </React.Fragment>
     )
 }
 
